c52a28f7304bfddc3db0cd89127473d5b1133e39
Sorikairo <sorikairox@gmail.com>
2023-12-07 16:50:54 +0000

    style: better readability

--- 2023/day07/step01.ts
+++ 2023/day07/step01.ts
@@ -31,43 +31,59 @@ export const getHandData = (hand: string) => {
   let [cards, bidAsString] = hand.split(' ');
   const bid = Number(bidAsString);
   const cardsOccurence: Record<string, number> = {};
+
   for (const card of cards) {
     if (!cardsOccurence[card]) {
       cardsOccurence[card] = 0;
     }
     cardsOccurence[card] += 1;
   }
+
   const distinctCardNumber = Object.keys(cardsOccurence).length;
+
   if (distinctCardNumber === 5) {
+
     return { rank: HAND_RANK.HIGH_CARD, cards, bid  }
+
   } else if (distinctCardNumber === 4) {
+
     return { rank: HAND_RANK.ONE_PAIR, cards, bid};
+
   } else if (distinctCardNumber === 3) {
+
     const cardsInHand = Object.keys(cardsOccurence);
     if (cardsOccurence[cardsInHand[0]] === 1 || cardsOccurence[cardsInHand[0]] === 2) {
       return { rank: HAND_RANK.TWO_PAIR, cards, bid};
     }
     return { rank: HAND_RANK.THREE_OF_A_KIND, cards, bid};
+
   } else if (distinctCardNumber === 2) {
+
     const cardsInHand = Object.keys(cardsOccurence);
+
     if (cardsOccurence[cardsInHand[0]] === 2 || cardsOccurence[cardsInHand[0]] === 3) {
       return { rank: HAND_RANK.FULL_HOUSE, cards, bid};
     }
     return { rank: HAND_RANK.FOUR_OF_A_KIND, cards, bid};
+
   }
+
   return { rank: HAND_RANK.FIVE_OF_A_KIND, cards, bid};
 }
 
 export const sortHands = (hands: { bid: number, cards: string, rank: number}[]) => {
     return hands.sort((a, b) => {
+
       if (a.rank !== b.rank) {
         return a.rank - b.rank;
       }
+
       for (let i = 0; i < a.cards.length; i++) {
         if (a.cards[i] !== b.cards[i]) {
           return CARD_VALUE[a.cards[i]] - CARD_VALUE[b.cards[i]]
         }
       }
+
       return 0;
     })
 }
@@ -75,14 +91,21 @@ export const sortHands = (hands: { bid: number, cards: string, rank: number}[]) 
 export const step01 = (input: string) => {
   const hands = [];
   const handsData = input.split('\n');
+
   for (let handData of handsData) {
     hands.push(getHandData(handData));
   }
+
   const sortedHands = sortHands(hands);
+
   let sum = 0;
-  for (let i = 1; i <= sortedHands.length;i ++) {
-    sum += (i * sortedHands[i - 1].bid);
+
+  for (let i = 1; i <= sortedHands.length;i++) {
+    const actualHand = sortedHands[i - 1];
+    sum += (i * actualHand.bid);
+    console.log(actualHand);
   }
+
   return sum;
 }
 
